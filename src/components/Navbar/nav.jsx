2f60be43780e53b071d0609a4968f86e6a81ca98
BlesonPhilip <blesonphilip21@gmail.com>
2025-03-18 20:48:06 +0530

    navbar and toggle is added with fully responsible

--- src/components/Navbar/nav.jsx
+++ src/components/Navbar/nav.jsx
@@ -1,33 +1,46 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./nav.css";
 
 const Nav = () => {
+  const menu = useRef();
+
+  const menuHandler = () => {
+    menu.current.classList.toggle("openMenu");
+    const isOpen = menu.current.classList.contains("openMenu");
+    document.getElementById("bars").setAttribute("aria-expanded", isOpen);
+  };
+
   return (
-    <>
-      <div className="navcss">
-        <div className="logo">
-          <a href="">Bleson Philip</a>
-        </div>
-        <ul>
-          <li>
-            <a href="">Home</a>
-          </li>
-          <li>
-            <a href="">About</a>
-          </li>
-          <li>
-            <a href="">Services</a>
-          </li>
-          <li>
-            <a href="">Portfolio</a>
-          </li>
-          <li>
-            <a href="">Contact</a>
-          </li>
-        </ul>
-        <i className="ri-menu-4-line"></i>
+    <nav className="navcss">
+      <div className="logo">
+        <a href="#">Bleson Philip</a>
       </div>
-    </>
+      <ul ref={menu}>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Services</a>
+        </li>
+        <li>
+          <a href="#">Portfolio</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+      <i
+        className="ri-menu-4-line"
+        id="bars"
+        onClick={menuHandler}
+        role="button"
+        aria-expanded="false"
+        tabIndex={0}
+      ></i>
+    </nav>
   );
 };
 
